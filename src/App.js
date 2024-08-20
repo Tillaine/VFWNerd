247875688d7f65b0ce4e8bc3532b61c1ff5509ed
Tillaine <55994239+Tillaine@users.noreply.github.com>
2024-08-20 17:46:43 -0600

    init mui style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
+import { ThemeProvider } from '@mui/material/styles';
 import logo from './assets/VFWNerdLogo.png';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ByLawsSearch from './components/ByLawsSearch'
 import ByawsTriviaButton from './components/BylawsTriviaButton'
 import AllStateChecklist from './components/AllStateCheckList'
+import GlobalThemeOverride from './theme/GlobalThemeOverride';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <Typography fontFamily={'Roboto'}>VFW Nerd</Typography>
-        <Box display="flex" flexDirection="row">
-          <ByLawsSearch/>
-          <ByawsTriviaButton/>
-          <AllStateChecklist/>
+      <GlobalThemeOverride>
+        <Box bgcolor='primary.light'>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+            <Typography variant='h2' color={'White'} fontFamily={'Roboto'}>
+              <Box display='flex' flexDirection='row'>
+              VFW
+               <a> 
+                <Box color="primary.main" > Nerd </Box>
+              </a>
+              </Box> 
+              </Typography>
+          <Box display="flex" flexDirection="row">
+            <ByLawsSearch/>
+            <ByawsTriviaButton/>
+            <AllStateChecklist/>
+          </Box>
+        </header>
         </Box>
-      </header>
+      </GlobalThemeOverride>
     </div>
   );
 }
