975ea9c0a553ab81ba345f6cd5eb2e4ef7ddc9a9
Tillaine <55994239+Tillaine@users.noreply.github.com>
2024-08-23 16:24:11 -0600

    reorginize

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-import { ThemeProvider } from '@mui/material/styles';
-import logo from './assets/VFWNerdLogo.png';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import ByLawsSearch from './components/ByLawsSearch'
-import ByawsTriviaButton from './components/BylawsTriviaButton'
-import AllStateChecklist from './components/AllStateCheckList'
 import GlobalThemeOverride from './theme/GlobalThemeOverride';
 import './App.css';
+
+import Home from './Home/Home'
 
 function App() {
   return (
     <div className="App">
       <GlobalThemeOverride>
-        <Box bgcolor='primary.light'>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <Typography variant='h2' color={'White'} fontFamily={'Roboto'}>
-              <Box display='flex' flexDirection='row'>
-              VFW
-               <a> 
-                <Box color="primary.main" > Nerd </Box>
-              </a>
-              </Box> 
-              </Typography>
-          <Box display="flex" flexDirection="row">
-            <ByLawsSearch/>
-            <ByawsTriviaButton/>
-            <AllStateChecklist/>
-          </Box>
-        </header>
-        </Box>
+        <Home/>
       </GlobalThemeOverride>
     </div>
   );
